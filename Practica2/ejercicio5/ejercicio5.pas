4976068016170f70fa874b447075b8434727e094
fabioo66 <torrejonfabio@gmail.com>
2024-04-01 18:32:26 -0300

    FOD

--- Practica2/ejercicio5/ejercicio5.pas
+++ Practica2/ejercicio5/ejercicio5.pas
@@ -1,8 +1,6 @@
 program ejercicio5;
 const valorAlto = 9999;
-const dimf = 30
-
-rango = 1..dimf;
+const dimf = 30;
 
 type
 	producto = record
@@ -14,6 +12,8 @@ type
 		precio : real;
 	end;
 	
+	rango = 1..dimf;
+	
 	venta = record
 		codigo : integer;
 		cantUnidades : integer;
@@ -23,6 +23,56 @@ type
 	detalle = file of venta;
 	vectorD = array[rango] of detalle; //vector detalles
 	vectorR = array[rango] of venta; //vector registros
+
+procedure importarMaestro(var mae : maestro);
+var
+	txt : text;
+	regM : producto;
+begin
+	assign(mae, 'maestro.dat');
+	rewrite(mae);
+	assign(txt, 'maestro.txt');
+	reset(txt);
+	while(not eof(txt))do begin
+		readln(txt, regM.codigo, regM.precio, regM.stockDisponible, regM.stockMinimo, regM.nombre);
+		readln(txt, regM.descripcion);
+		write(mae, regM);
+	end;
+	writeln('Archivo maestro creado');
+	close(mae);
+	close(txt);
+end;
+
+procedure importarDetalle(var det : detalle);
+var
+	rutaBinario, rutaTxt : string;
+    txt2 : text;
+    regD : venta;
+begin
+	writeln('Ingrese la ruta del archivo detalle binario');
+	readln(rutaBinario);
+    assign(det, rutaBinario);
+    rewrite(det);
+    writeln('Ingrese la ruta del archivo detalle.txt');
+	readln(rutaTxt);
+    assign(txt2, rutaTxt);
+    reset(txt2);
+    while(not eof(txt2)) do begin
+        readln(txt2, regD.codigo, regD.cantUnidades); 
+        write(det, regD);
+    end;
+    writeln('Archivo detalle creado');
+    close(det);
+    close(txt2);
+end;
+
+procedure cargarVectorDetalles(var vd : vectorD);
+var
+	i : rango;
+begin
+	for i := 1 to dimf do
+		importarDetalle(vd[i]);
+end;
 	
 procedure leer(var det : detalle; regD : venta);
 begin
@@ -38,21 +88,14 @@ var
 begin
 	min.codigo := valorAlto;
 	for i := 1 to dimf do begin
-		if(vr[i].codigo < min)then begin
-			min := vr[i].codigo;
+		if(vr[i].codigo < min.codigo)then begin
+			min.codigo := vr[i].codigo;
 			pos := i;
+		end;
 		if(min.codigo <> valorAlto)then
 			leer(vd[pos], vr[pos]);
 	end;
 end; 
-
-procedure actualizarMaestro(var mae : maestro; var vd : vectorD);
-var
-begin
-	reset
-end;
-
-
 
 procedure reporte(var mae : maestro);
 var
@@ -75,11 +118,64 @@ begin
 	close(txt);
 end;
 
-		
+procedure actualizarMaestro(var mae : maestro; var vd : vectorD);
 var
+	min : venta;
+	regM : producto;
+	vr : vectorR;
+	codigo, cant, i : integer;
+begin
+	reset(mae);
+	read(mae, regM);
+	for i:= 1 to dimf do begin
+		reset(vd[i]);
+		leer(vd[i], vr[i]);
+	end;
+	minimo(vd, vr, min);
+	while(min.codigo <> valorAlto)do begin
+		codigo := min.codigo;
+		cant := 0;
+		while(min.codigo = codigo)do begin
+			cant := cant + min.cantUnidades; 
+			minimo(vd, vr, min);
+		end;
+		while(regM.codigo <> codigo)do
+			read(mae, regM);
+		regM.stockDisponible := regM.stockDisponible - cant;
+		seek(mae, filepos(mae)-1);
+		write(mae, regM);
+	end;
+	writeln('Maestro actualizado');		
+	reporte(mae);
+	close(mae);
+	for i:= 1 to dimf do
+		close(vd[i]);
+end;
 
+procedure imprimirProducto(p : producto);
+begin
+	writeln('Codigo= ', p.codigo, ' precio= ', p.precio:0:2, ' stock actual ', p.stockDisponible, ' stock minimo ', p.stockMinimo, ' nombre ', p.nombre, ' descripcion ', p.descripcion);
+end;
+		
+procedure imprimirMaestro(var mae : maestro);
+var
+	p : producto;
+begin
+	reset(mae);
+	while(not eof(mae))do begin
+		read(mae, p);
+		imprimirProducto(p);
+	end;
+end;
+
+var
+	mae : producto;
+	vd : vectorD;
+	i : rango;
 BEGIN
-	
-	
+	importarMaestro(mae);
+	cargarVectorDetalles(vd);
+	actualizarMaestro(mae, vd);
+	imprimirMaestro(mae);
 END.
 
