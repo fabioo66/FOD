fd96e5aea24ca39d5c8eb05abb2619be1ac4a404
fabioo66 <torrejonfabio@gmail.com>
2024-04-09 17:13:36 -0300

    FOD

--- Practicas resueltas/Practica2/ejercicio13/ejercicio13.pas
+++ Practicas resueltas/Practica2/ejercicio13/ejercicio13.pas
@@ -1,5 +1,26 @@
+{Una compañía aérea dispone de un archivo maestro donde guarda información sobre sus
+próximos vuelos. En dicho archivo se tiene almacenado el destino, fecha, hora de salida y la
+cantidad de asientos disponibles. La empresa recibe todos los días dos archivos detalles
+para actualizar el archivo maestro. En dichos archivos se tiene destino, fecha, hora de salida
+y cantidad de asientos comprados. Se sabe que los archivos están ordenados por destino
+más fecha y hora de salida, y que en los detalles pueden venir 0, 1 ó más registros por cada
+uno del maestro. Se pide realizar los módulos necesarios para:
+a.
+Actualizar el archivo maestro sabiendo que no se registró ninguna venta de pasaje
+sin asiento disponible.}
+
 program ejercicio13;
 type
+	vuelo = record
+		destino : string;
+		fecha : string;
+		hora : real;
+		cantAsientos : integer;
+	end;
+	
+	dato = rec
+	
+	
 	
 
 var
