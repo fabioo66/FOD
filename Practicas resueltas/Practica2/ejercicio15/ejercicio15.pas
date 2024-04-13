c54b49f0e114114976db07eee7d8aee1d12c9592
fabioo66 <torrejonfabio@gmail.com>
2024-04-13 16:04:58 -0300

    FOD

--- Practicas resueltas/Practica2/ejercicio15/ejercicio15.pas
+++ Practicas resueltas/Practica2/ejercicio15/ejercicio15.pas
@@ -15,6 +15,7 @@ ventas de semanarios si no hay ejemplares para hacerlo}
 program ejercicio15;
 const
 	valorAlto = 'ZZZZ';
+	dimf = 3; // = 100;
 type
 	rango = 1..dimf;
 	
@@ -27,6 +28,68 @@ type
 		total : integer;
 		totalVendidos : integer;
 	end;
+	
+	venta = record
+		fecha : string;
+		codigo : integer;
+		cant : integer;
+	end;
+	
+	maestro = file of emision;
+	detalle = file of venta;
+	
+	vectorD = array[rango]of detalle;
+	vectorR = array[rango]of venta;
+	
+procedure importarMaestro(var mae : maestro);
+var
+	txt : text;
+	regM : archivoMaestro;
+begin
+	assign(mae, 'maestro.dat');
+	rewrite(mae);
+	assign(txt, 'maestro.txt');
+	reset(txt);
+	while(not eof(txt))do begin
+		readln(txt, regM.codigoProv, regM.nombreProv);
+		readln(txt, regM.codigoLoc, regM.sinLuz, regM.sinGas, regM.deChapa, regM.sinAgua, regM.sinSanitarios, regM.nombreLoc);
+		write(mae, regM);
+	end;
+	writeln('Archivo maestro creado');
+	close(mae);
+	close(txt);
+end;
+
+procedure importarDetalle(var det : detalle);
+var
+	ruta : string;
+    txt2 : text;
+    regD : archivoDetalle;
+begin
+	writeln('Ingrese la ruta del archivo detalle binario');
+	readln(ruta);
+    assign(det, ruta);
+    rewrite(det);
+    writeln('Ingrese la ruta del archivo detalle.txt');
+	readln(ruta);
+    assign(txt2, ruta);
+    reset(txt2);
+    while(not eof(txt2)) do begin
+        readln(txt2, regD.codigoProv, regD.codigoLoc, regD.conLuz, regD.construidas, regD.conAgua, regD.conGas, regD.sanitarios);
+        write(det, regD);
+    end;
+    writeln('Archivo detalle creado');
+    close(det);
+    close(txt2);
+end;
+
+procedure cargarVectorDetalles(var vd : vectorD);
+var
+	i : rango;
+begin
+	for i := 1 to dimf do
+		importarDetalle(vd[i]);
+end;
 	
 
 var
